5f32055cea3a0aa16c69e59b8d7557521fff6e2a
apsteinberg <asherps@gmail.com>
2025-07-18 23:30:00 -0400

    setting up proper test dataset for pipeline

--- tests/test.sh
+++ tests/test.sh
@@ -4,6 +4,10 @@ source $HOME/miniforge3/bin/activate ${env_nf}
 ## specify params
 outdir=$HOME/Library/CloudStorage/OneDrive-MemorialSloanKetteringCancerCenter/A673/APS010.1_pg3_nf_test/results
 pipelinedir=$HOME/VSCodeProjects/proteomegenerator3
+samplesheet=${pipelinedir}/assets/local_samplesheet.csv
+test_data=$HOME/Library/CloudStorage/OneDrive-MemorialSloanKetteringCancerCenter/A673/APS010.1_pg3_nf_test/test_data
+ref_genome=${test_data}/Homo_sapiens.GRCh38.dna_sm.primary_assembly_chr9_1_1000000.fa
+gtf=${test_data}/Homo_sapiens.GRCh38.91_chr9_1_1000000.gtf
 mkdir -p ${outdir}
 cd ${outdir}
 
